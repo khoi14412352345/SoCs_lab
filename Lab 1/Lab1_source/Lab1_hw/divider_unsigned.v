d696d72ee2c1ba9329cf9fae7394c0df37cef742
khoylmfao <92151725+khoi14412352345@users.noreply.github.com>
2025-12-11 22:59:20 +0700

    Update divider_unsigned.v

--- Lab 1/Lab1_source/Lab1_hw/divider_unsigned.v
+++ Lab 1/Lab1_source/Lab1_hw/divider_unsigned.v
@@ -20,6 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
+
 module divider_unsigned (
     input  [31:0] i_dividend,     // was: dividend
     input  [31:0] i_divisor,      // was: divisor
@@ -41,15 +42,16 @@ module divider_unsigned (
     generate
         for (i = 0; i < 32; i = i + 1) begin : iter
             divu_1iter u_iter (
-                .dividend_in  (dividend_stage[i]),
-                .divisor      (i_divisor),
-                .quotient_in  (quotient_stage[i]),
-                .remainder_in (remainder_stage[i]),
+    .i_dividend_in (dividend_stage[i]),
+    .i_divisor     (i_divisor),
+    .i_quotient_in (quotient_stage[i]),
+    .i_remainder_in(remainder_stage[i]),
 
-                .dividend_out (dividend_stage[i+1]),
-                .quotient_out (quotient_stage[i+1]),
-                .remainder_out(remainder_stage[i+1])
-            );
+    .o_dividend_out(dividend_stage[i+1]),
+    .o_quotient_out(quotient_stage[i+1]),
+    .o_remainder_out(remainder_stage[i+1])
+);
+
         end
     endgenerate
 
@@ -58,3 +60,41 @@ module divider_unsigned (
     assign o_remainder = remainder_stage[32];
 
 endmodule
+
+module divu_1iter (
+    input  [31:0] i_dividend_in,
+    input  [31:0] i_divisor,
+    input  [31:0] i_quotient_in,
+    input  [31:0] i_remainder_in,
+
+    output [31:0] o_dividend_out,
+    output [31:0] o_quotient_out,
+    output [31:0] o_remainder_out
+);
+
+    // remainder' = (remainder << 1) | MSB(dividend)
+    wire [31:0] rem_shift;
+    assign rem_shift = {i_remainder_in[30:0], i_dividend_in[31]};
+
+    // dividend' = dividend << 1
+    assign o_dividend_out = {i_dividend_in[30:0], 1'b0};
+
+    // so sánh remainder' với divisor
+    wire rem_lt_div;
+    assign rem_lt_div = (rem_shift < i_divisor);
+
+    // quotient' = shift left, thêm bit 0 hoặc 1 tùy theo so sánh
+    assign o_quotient_out =
+        rem_lt_div ?
+            (i_quotient_in << 1) :
+            ((i_quotient_in << 1) | 32'h1);
+
+    // remainder' = rem_shift hoặc rem_shift - divisor
+    assign o_remainder_out =
+        rem_lt_div ?
+            rem_shift :
+            (rem_shift - i_divisor);
+
+endmodule
+
+
